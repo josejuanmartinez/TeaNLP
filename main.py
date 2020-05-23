d25759308da80be820bfd9dcbc6ede60272cf101
Jose J. Martinez <josejuan.martinez@taiger.com>
2020-05-23 13:43:13 +0200

    Adds connection to Neo4J

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from py2neo import Graph
 
 app = Flask(__name__)
+graph = Graph("bolt://localhost:7687", auth=("TeaNLP", "teanlp"))
 
 
 @app.route("/")
 def hello():
+    graph.run("Match () Return 1 Limit 1")
     return 'Hello, Amparo!'
+
